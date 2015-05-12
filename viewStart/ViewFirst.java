26ad71d04288bcd0ccd1f9d6e077e21ebe822fc2
Lewis GodGiven ( Portable ) <Lewis@172.19.143.246>
2015-05-12 17:23:12 -0400

    Visuel de la fenêtre de démarrage OK Lancement des autres views en cours

--- viewStart/ViewFirst.java
+++ viewStart/ViewFirst.java
@@ -21,7 +21,7 @@ public class ViewFirst extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Démarrage de GodWay Chat");
+        primaryStage.setTitle("GodWay Chat");
         primaryStage.setResizable(false);
         Group root = new Group();
         Scene scene = new Scene(root, 500, 300);
@@ -34,48 +34,86 @@ public class ViewFirst extends Application {
         });        
         
         // titre
-        Text title = new Text("Coucou PD");
-        title.setX(60);
-        title.setY(20);
-        title.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
+        Text title = new Text("Bienvenue dans le GodWay Chat!");
+        title.setX(80);
+        title.setY(40);
+        title.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
         
-        // bouton fermer serv
-        Button btnServer = new Button();
-        /*btnServer.setLayoutX(384);
-        btnServer.setLayoutY(4);
-        btnServer.setStyle("-fx-font-size: 8pt;");
-        btnServer.setMinHeight(22.0);
-        btnServer.setPrefHeight(22.0);
-        btnServer.setMaxHeight(22.0); */
-        btnServer.setText("Serveur");
-        btnServer.setOnAction(new EventHandler<ActionEvent>() {
-        // action du bouton
-        public void handle(ActionEvent event) {
-                System.out.println("Fermeture du serveur. Bye bye!");
+        // descrption sous le titre
+        Text titleDesc = new Text("Choisissez si vous voulez démarrer un serveur ou un client");
+        titleDesc.setX(45);
+        titleDesc.setY(60);
+        titleDesc.setFont(Font.font("Verdana", FontWeight.NORMAL, 14));
+        
+        // Description serveur
+        Text servDesc = new Text("Configurer un serveur");
+        servDesc.setX(285);
+        servDesc.setY(240);
+        servDesc.setFont(Font.font("Verdana", FontWeight.NORMAL, 14));
+        
+        // Description client
+        Text servClient = new Text("Ouvrir un client");
+        servClient.setX(80);
+        servClient.setY(240);
+        servClient.setFont(Font.font("Verdana", FontWeight.NORMAL, 14));
+                
+        // bouton CLIENT
+        Button btnClient = new Button();
+        btnClient.setText("Client");
+        
+        btnClient.setLayoutX(70);
+        btnClient.setLayoutY(100);
+        
+        btnClient.setStyle("-fx-font-size: 16pt;");
+        
+        btnClient.setMinHeight(120.0);
+        btnClient.setPrefHeight(120.0);
+        btnClient.setMaxHeight(120.0);
+        
+        btnClient.setMinWidth(140.0);
+        btnClient.setPrefWidth(140.0);
+        btnClient.setMaxWidth(140.0);
+        
+        btnClient.setOnAction(new EventHandler<ActionEvent>() {
+        	// action du bouton
+        	public void handle(ActionEvent event) {
+        	// TODO fermeture de tous les threads
                 System.exit(0);
             }
         });
         
-        // bouton chat
-        Button btnClient = new Button();
-        /*btnClient.setLayoutX(403);
-        btnClient.setLayoutY(275);*/
-        btnClient.setText("Client");
-        btnClient.setOnAction(new EventHandler<ActionEvent>() {
-        // action du bouton
-        public void handle(ActionEvent event) {
-
+        // bouton SERVEUR
+        Button btnServer = new Button();
+        btnServer.setText("Serveur");
+        
+        btnServer.setLayoutX(290);
+        btnServer.setLayoutY(100);
+        
+        btnServer.setStyle("-fx-font-size: 16pt;");
+        
+        btnServer.setMinHeight(120.0);
+        btnServer.setPrefHeight(120.0);
+        btnServer.setMaxHeight(120.0);
+        
+        btnServer.setMinWidth(140.0);
+        btnServer.setPrefWidth(140.0);
+        btnServer.setMaxWidth(140.0);
+        
+        btnServer.setOnAction(new EventHandler<ActionEvent>() {
+        	// action du bouton
+        	public void handle(ActionEvent event) {
+        	// TODO fermeture de tous les threads
+                System.exit(0);
             }
         });
         
-        VBox vBox = new VBox();
-        vBox.setSpacing(10);
-        vBox.setPadding(new Insets(0, 20, 10, 20));
-        vBox.getChildren().addAll(btnClient, btnServer);
-        
         // ajout de tous les items à la scene
         root.getChildren().add(title);
-        root.getChildren().add(vBox);
+        root.getChildren().add(titleDesc);
+        root.getChildren().add(servDesc);
+        root.getChildren().add(servClient);
+        root.getChildren().add(btnClient);
+        root.getChildren().add(btnServer);
         primaryStage.setScene(scene);
        
         primaryStage.show();
